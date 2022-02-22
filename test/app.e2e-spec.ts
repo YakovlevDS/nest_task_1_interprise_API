4b5cec37e27482a7d73f0ab30bdfa4ce06040ef4
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-02-22 19:27:48 +0200

    feat:add get test e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -33,5 +40,27 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
+  });
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+
+    it.todo('DELETE');
+    it.todo('PATCH');
+  });
+
+  it('POST', () => {
+    return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Cards, money...!',
+        year: 1998,
+        genres: ['criminal', 'camedy', 'action'],
+      })
+      .expect(201);
   });
 });
