e3e04d2d80d739484f8d604716460dc532a063c9
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-02-22 17:52:58 +0200

    feat:add test func getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -20,6 +21,26 @@ describe('MoviesService', () => {
     it('should return array', () => {
       const result = service.getAll();
       expect(result).toBeInstanceOf(Array);
+    });
+  });
+  describe('Testing function getOne', () => {
+    it('should return  movie', () => {
+      service.create({
+        title: 'Cards, money...!!',
+        year: 1998,
+        genres: ['Test genres'],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined;
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should return  404 error', () => {
+      try {
+        const movie = service.getOne(9999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
